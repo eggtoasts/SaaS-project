b5531865a6ccae26282ce1ff918dee7dcbfb6a76
eggtoasts <129806552+eggtoasts@users.noreply.github.com>
2025-10-29 16:24:24 -0400

    Add navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,11 @@ import Layout from "./pages/Layout";
 import Dashboard from "./pages/Dashboard";
 import WriteArticle from "./pages/WriteArticle";
 import BlogTitles from "./pages/BlogTitles";
+import RemoveObject from "./pages/RemoveObject";
+import RemoveBackground from "./pages/RemoveBackground";
+import GenerateImages from "./pages/GenerateImages";
+import Community from "./pages/Community";
+import ReviewResume from "./pages/ReviewResume";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +22,11 @@ const router = createBrowserRouter([
       { index: true, element: <Dashboard /> },
       { path: "write-article", element: <WriteArticle /> },
       { path: "blog-titles", element: <BlogTitles /> },
+      { path: "generate-images", element: <GenerateImages /> },
+      { path: "remove-background", element: <RemoveBackground /> },
+      { path: "remove-object", element: <RemoveObject /> },
+      { path: "review-resume", element: <ReviewResume /> },
+      { path: "community", element: <Community /> },
     ],
   },
 ]);
